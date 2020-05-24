f40190e652b4784998389eab2c1ab173bb5eb98f
Iain Earl <iain@itmecho.com>
2020-05-24 14:36:55 +0100

    Add simple CLI interface for templating files

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+#[macro_use]
+extern crate clap;
+
+use anyhow::{Context, Result};
+use clap::{App, Arg};
+use germinate::Seed;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let matches = App::new("germinate")
+        .about("Template files using values from various sources")
+        .author(crate_authors!())
+        .version(crate_version!())
+        .arg(Arg::with_name("INPUT_FILE").required(true))
+        .arg(
+            Arg::with_name("output-file")
+                .short("o")
+                .long("output-file")
+                .takes_value(true)
+                .required(false),
+        )
+        .get_matches();
+
+    // Safe to unwrap the input file as it's a required argument
+    let input = std::fs::read_to_string(matches.value_of("INPUT_FILE").unwrap())
+        .context("Failed to read input file")?;
+
+    let mut seed = Seed::new(input);
+    let output = seed.germinate().await?;
+
+    // If no output file is given, write the output to stdout
+    match matches.value_of("output-file").unwrap_or("-") {
+        "-" => print!("{}", output),
+        path => std::fs::write(path, output)?,
+    };
+
+    Ok(())
+}
